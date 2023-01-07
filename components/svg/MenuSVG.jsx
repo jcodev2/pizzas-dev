7ee1132d79fbc6151d275ed32807043bf4b16d98
Jeancarlo Morales <jcode2006@gmail.com>
2023-01-07 11:21:41 -0500

    Change svg icons to react component

---
+++ components/svg/MenuSVG.jsx
@@ -0,0 +1,17 @@
+const MenuSVG = (props) => (
+  <svg
+    width={24}
+    height={24}
+    viewBox='0 0 16 16'
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    {...props}
+  >
+    <path
+      d='M1 3.75A.75.75 0 0 1 1.75 3h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 3.75zm0 4A.75.75 0 0 1 1.75 7h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 7.75zM1.75 11a.75.75 0 0 0 0 1.5h12.5a.75.75 0 0 0 0-1.5H1.75z'
+      fill='#000'
+    />
+  </svg>
+)
+
+export default MenuSVG
