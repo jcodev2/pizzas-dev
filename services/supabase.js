6932d9d1a27a7c2114ae94f0a96c5c24c45f15cc
Jeancarlo Morales <jcode2006@gmail.com>
2023-01-07 11:19:43 -0500

    Add supabase client

--- services/supabase.js
+++ services/supabase.js
@@ -6,9 +6,3 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
-
-/* async function signInWithGitHub() {
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: 'github'
-  })
-} */
