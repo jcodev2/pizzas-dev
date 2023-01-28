9420cc65e48b55ccfcab14c55c154cf656c7d915
Jeancarlo Morales <jcode2006@gmail.com>
2023-01-28 12:08:23 -0500

    add supabase service

--- services/supabase.js
+++ services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://aroymlgjkmfbqdrnsjch.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyb3ltbGdqa21mYnFkcm5zamNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI3NzA3ODgsImV4cCI6MTk4ODM0Njc4OH0.aQcuSSFQvA3AiBm-oyP7NUac5dyseO8BWAWY3XyXwh0'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
