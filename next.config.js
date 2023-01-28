21d2aacf9afaacad283187ed592ff35dc3715836
Jeancarlo Morales <jcode2006@gmail.com>
2023-01-28 12:10:51 -0500

    update domains in next.confing.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',
+      'openai-labs-public-images-prod.azureedge.net'
+    ]
+  }
 }
 
 module.exports = nextConfig
