d4eb04c3eb6085403e16fa16094fa7a2c8e99984
Jeancarlo Morales <jcode2006@gmail.com>
2023-02-23 23:12:26 -0500

    Add provider to wrap entire project in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import { CartProvider } from 'context/cart'
 import '../styles/main.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  )
 }
