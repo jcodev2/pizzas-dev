3cce05a2c8027562624b2b0a0f0ee407a496c7d7
Jeancarlo Morales <jcode2006@gmail.com>
2023-02-23 23:17:36 -0500

    Add project-wide context provider and other improvements Description This pull request includes various updates to the project, including:
    A new component Navigation in the components folder.
    Indentation changes to the Layout component in the components folder.
    A new component ButtonCounter in the components folder.
    Added a priority property to the Image component in the Header component in the components folder.
    Added new SVG components in the components/svg folder.
    Created a new CartContext in the context folder.
    Created a new useUser hook in the hooks folder with a getURL function and updated the home page to allow access only if the user is logged in.
    Added new styles to the main.css file in the styles folder.
    Added a new test file addZero.test.js in the tests folder.
    Changes Made
    Added new components, context, hooks, and styles.
    Modified existing components and tests.
    Reason for Changes
    These changes were made to improve the functionality and user experience of the project.
    
    Related Issues
    No related issues were addressed in this pull request.

---
+++ utilities/crud/readMenu.js
@@ -0,0 +1,27 @@
+import supabase from 'services/supabase'
+
+// get the menu from the supbase database
+const readMenu = async () => {
+  try {
+    const { data: menu, error } = await supabase.from('pizzas').select('*')
+
+    return [menu, error]
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const readSixPizzas = async () => {
+  try {
+    const { data: menu, error } = await supabase
+      .from('pizzas')
+      .select('*')
+      .limit(6)
+
+    return [menu, error]
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export { readMenu, readSixPizzas }
