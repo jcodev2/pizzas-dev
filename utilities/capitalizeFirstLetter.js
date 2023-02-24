df1249d243656f2e380a30af6d68e5361afb1f6c
Jeancarlo Morales <jcode2006@gmail.com>
2023-02-23 23:06:35 -0500

    Refactor capitalizeFirstLetter utility function

--- utilities/capitalizeFirstLetter.js
+++ utilities/capitalizeFirstLetter.js
@@ -1,4 +1,11 @@
 const capitalizeFirstLetter = (string) => {
+  if (typeof string !== 'string') {
+    throw new Error('The argument must be a string')
+  }
+  if (Number.isNaN(string)) {
+    throw new Error('The argument must be a string')
+  }
+
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
