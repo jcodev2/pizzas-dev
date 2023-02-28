44d1f290e1f32cf6b7e8246ae94bee448818a3b0
Jeancarlo Morales <jcode2006@gmail.com>
2023-02-28 17:11:31 -0500

    Add function to store data in localStorage

--- context/cart.jsx
+++ context/cart.jsx
@@ -14,10 +14,18 @@ export const CartProvider = ({ children }) => {
       return item
     })
     const isPizzaInCart = newCart.find((item) => item.id === pizza.id)
+
     if (isPizzaInCart) {
       setCart(newCart)
+
+      localStorage.setItem('cart', JSON.stringify(newCart))
     } else {
       setCart([...cart, { ...pizza, quantity: 1 }])
+
+      localStorage.setItem(
+        'cart',
+        JSON.stringify([...cart, { ...pizza, quantity: 1 }])
+      )
     }
   }, 500)
 
@@ -31,13 +39,22 @@ export const CartProvider = ({ children }) => {
     const isPizzaInCart = newCart.find((item) => item.id === pizza.id)
     if (isPizzaInCart.quantity > 0) {
       setCart(newCart)
+
+      localStorage.setItem('cart', JSON.stringify(newCart))
     } else {
       setCart(cart.filter((item) => item.id !== pizza.id))
+
+      localStorage.setItem(
+        'cart',
+        JSON.stringify(cart.filter((item) => item.id !== pizza.id))
+      )
     }
   }, 500)
 
   const clearCart = debounce(() => {
     setCart([])
+
+    localStorage.setItem('cart', JSON.stringify([]))
   }, 500)
 
   return (
