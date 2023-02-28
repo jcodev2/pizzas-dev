1c2f969eee7ca86e3486236a8c307c6939b6c6a9
Jeancarlo Morales <jcode2006@gmail.com>
2023-02-28 17:08:07 -0500

    Fix identation in MyLink component

--- components/myLink.jsx
+++ components/myLink.jsx
@@ -1,12 +1,16 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import React from 'react'
 
 const MyLink = ({ href, children }) => {
   const router = useRouter()
   const isActived = router.pathname === href
 
   return (
-    <Link href={href} className={isActived ? 'active' : ''}>
+    <Link
+      href={href}
+      className={isActived ? 'active' : ''}
+    >
       {children}
     </Link>
   )
