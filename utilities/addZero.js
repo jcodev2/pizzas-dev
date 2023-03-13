9c515bd5720a944fa2cd915249c177dcbd00a85a
Jeancarlo Morales <jcode2006@gmail.com>
2023-03-13 16:26:56 -0500

    Refactor addZero utility

--- utilities/addZero.js
+++ utilities/addZero.js
@@ -1,5 +1,14 @@
 const addZero = (num) => {
-  return num < 10 ? `${num}.0` : num
+  const numCheck = Number.isInteger(num) ? parseInt(num) : parseFloat(num)
+
+  // check if the num is integer and less than 10
+  if (Number.isInteger(numCheck) && num < 10) return `${num}.0`
+
+  // check if the num is float and less than 10
+  if (numCheck < 10) return `${num}`
+
+  // check if the num is greater than 10
+  if (numCheck > 10) return `${num}`
 }
 
 export default addZero
